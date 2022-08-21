70b6e313e182c80ce305b448352186b7e4ff4e14
Daria <poborskaiadv2@icloud.com>
2022-08-21 22:46:47 +0300

    Домашнее задание к семинару 4

---
+++ HT3_sem4/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]  
+
+int []array = {1, 2, 5, 7, 19, 6, 1, 33};
+
+void PrintArray(int []arr)
+{
+    int count = arr.Length;//в нашем случае count = 8
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine();
+}
+
+PrintArray(array);
