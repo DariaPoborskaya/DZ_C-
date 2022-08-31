39bde5e4bb4ff524833cadd324861ee19cb4f928
Daria <poborskaiadv2@icloud.com>
2022-08-31 18:03:34 +0300

    Задача 54 Семинар 8 ДЗ

---
+++ HT1_Sem8/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] CreateMtrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(min, max + 1);
+        }
+    }
+
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],3},");
+            else Console.Write($"{matrix[i, j],3} ");
+        }
+        Console.WriteLine("] ");
+    }
+}
+
+void SelectionSort(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+           for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+           {
+            if (matrix[i, k] < matrix[i, k + 1])
+            {
+                int temp = matrix[i, k + 1];
+          matrix[i, k + 1] = matrix[i, k];
+          matrix[i, k] = temp;
+            }
+           }
+        }
+    }
+}
+
+int[,] array2d = CreateMtrixRndInt(3, 4, 1, 10);
+PrintMatrix(array2d);
+SelectionSort(array2d);
+Console.WriteLine();
+PrintMatrix(array2d);
